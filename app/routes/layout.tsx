4a1d7bb49587d87314f25b89a4ae2bac57dffd05
ucyuta <yuta@yutasaba.com>
2026-01-31 19:57:57 +0900

    fix: ヘッダーを変更、レイアウトを変更

---
+++ app/routes/layout.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { Outlet } from "react-router";
+import Header from "~/components/header";
+
+export default function Layout() {
+    return (
+        <>
+            <Flex minH="svh" flexDir="column">
+                <Header/>
+
+                <Flex as="main" flex="1" flexDir="column">
+                    <Outlet/>
+                </Flex>
+            </Flex>
+        </>
+    )
+}
