ed7b2bf42849391431b8402836137819bd172df9
ucyuta <yuta@yutasaba.com>
2026-01-31 20:00:09 +0900

    fix: 404ページのレイアウトを変更

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -1,31 +1,30 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Center, Container, Flex, Span, Text } from "@chakra-ui/react";
 
 export default function PageNotFound() {
   return (
-    <Flex
-      alignItems="center"
-      justify="center"
-      mt="10"
-    >
-      <Box>
-        <Text 
-          fontSize="9xl" 
-          fontFamily="Lato" 
-          letterSpacing="0.05em" 
-          textShadow="0 0 4px gray" 
-          fontWeight="semibold"
-          textAlign="center"
-        >
-          404
-        </Text>
-        <Text 
-          fontSize="4xl"
-          textAlign="center"
-          fontFamily="mono"
-        >
-          Not Found
-        </Text>
-      </Box>
-    </Flex>
+    <Container asChild>
+      <Flex
+        flex="1"
+        align="center"
+        justify="center" 
+      >
+        <Center>
+          <Text 
+            as="h1"
+            fontSize="9xl" 
+            fontFamily="Lato" 
+            letterSpacing="0.05em" 
+            textShadow="0 0 3px gray" 
+            fontWeight="semibold"
+            textAlign="center"
+          >
+            404
+            <Span pl="5" fontWeight="normal" fontSize="4xl" fontFamily="mono">
+              Not Found
+            </Span>
+          </Text>
+        </Center>
+      </Flex>
+    </Container>
   )
 }
